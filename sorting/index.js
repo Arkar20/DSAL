bc090c66d103dd9da1e7c620ed64ba516761e608
arkar <arkar20011@gmail.com>
2022-03-12 19:42:17 +0630

    bubble and selection sort

--- sorting/index.js
+++ sorting/index.js
@@ -1,11 +1,38 @@
 // Implement a swap helper function that we will use in both BS and SS
-function swap(arr, i, j) {}
+function swap(arr, i, j) {
+  let temp = arr[j];
+  arr[j] = arr[i];
+  arr[i] = temp;
+}
 
 //bubbleSort works by having sorted data accumulate at end of array
-function bubbleSort(arr) {}
+function bubbleSort(arr) {
+  for (let j = 0; j < arr.length; j++){
+      for (let i = 0; i <= arr.length-j; i++){
+    if (arr[i] > arr[i+1]){
+      swap(arr, i, i+1);
+    }
+  }
+  }
+
+  return arr;
+
+}
 
 //selectionSort works by having sorted data accumulate at start of array
-function selectionSort(arr) {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++){
+    let swap_idx = i;
+    for (let j = i+1; j < arr.length; j++) {
+      if (arr[j] < arr[swap_idx]) {
+        swap_idx=j;
+      }
+    }
+        swap(arr, i, swap_idx);
+
+  }
+  return arr
+}
 
 // _________ _______  _______ _________   _______  _______  _______  _______  _______
 // \__   __/(  ____ \(  ____ \\__   __/  (  ____ \(  ___  )(  ____ \(  ____ \(  ____ \
@@ -41,13 +68,13 @@ describe("swap()", () => {
   });
 });
 
-describe.skip("Bubble Sort", () => {
+describe("Bubble Sort", () => {
   it("sorts an array", () => {
     assert.deepEqual(bubbleSort([5, 1, 3, 7, 6, 2, 4]), [1, 2, 3, 4, 5, 6, 7]);
   });
 });
 
-describe.skip("Selection Sort", () => {
+describe("Selection Sort", () => {
   it("sorts an array", () => {
     assert.deepEqual(selectionSort([5, 1, 3, 2, 4]), [1, 2, 3, 4, 5]);
   });
